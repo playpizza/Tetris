b8d6bd3450e4e46fb522868a39657e94a235a142
playpizza <39541376+playpizza@users.noreply.github.com>
2021-03-13 23:07:21 +0900

    테트리스 배경 생성
    타일보드, 좌우, 하단 테두리

--- Tetris/Assets/Scripts/Stage.cs
+++ Tetris/Assets/Scripts/Stage.cs
@@ -16,4 +16,65 @@ public class Stage : MonoBehaviour
     [Range(5, 20)]
     public int boardHeight = 20;
     public float fallCycle = 1.0f;
+
+    private int halfWidth;
+    private int halfHeight;
+
+    private void Start()
+    {
+        halfWidth = Mathf.RoundToInt(boardWidth * 0.5f);
+        halfHeight = Mathf.RoundToInt(boardHeight * 0.5f);
+
+        CreateBackground();
+        CreateTetromino();
+    }
+
+    // 배경 타일 생성
+    Tile CreateTile(Transform parent, Vector2 position, Color color, int order = 1)
+    {
+        var go = Instantiate(tilePrefab);
+        go.transform.parent = parent;
+        go.transform.localPosition = position;
+
+        var tile = go.GetComponent<Tile>();
+        tile.color = color;
+        tile.sortingOrder = order;
+
+        return tile;
+    }
+
+    void CreateBackground()
+    {
+        Color color = Color.gray;
+
+        // 타일 보드
+        color.a = 0.5f;
+        for(int x = -halfWidth; x < halfWidth; ++x)
+        {
+            for(int y = halfHeight; y > -halfHeight; --y)
+            {
+                CreateTile(backgroundNode, new Vector2(x, y), color, 0);
+            }
+        }
+
+        // 좌 우 테두리
+        color.a = 1.0f;
+        for(int y = halfHeight; y > -halfHeight; --y)
+        {
+            CreateTile(backgroundNode, new Vector2(-halfWidth - 1, y), color, 0);
+            CreateTile(backgroundNode, new Vector2(halfWidth, y), color, 0);
+        }
+
+        // 하단 테두리
+        for(int x = -halfWidth - 1; x <= halfWidth; ++x)
+        {
+            CreateTile(backgroundNode, new Vector2(x, -halfHeight), color, 0);
+        }
+    }
+
+    // 테트리스 타일 (테트로미노) 생성
+    void CreateTetromino()
+    {
+        
+    }
 }
