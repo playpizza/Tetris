f06eab056e7eb60c5ac51911e90716955c679e19
playpizza <39541376+playpizza@users.noreply.github.com>
2021-03-15 23:52:27 +0900

    테트리스 블록
    테트리스블록 7종류

--- Tetris/Assets/Scripts/Stage.cs
+++ Tetris/Assets/Scripts/Stage.cs
@@ -75,6 +75,76 @@ public class Stage : MonoBehaviour
     // 테트리스 타일 (테트로미노) 생성
     void CreateTetromino()
     {
-        
+        int index = Random.Range(0, 7);
+        Color32 color = Color.white;
+
+        tetrominoNode.rotation = Quaternion.identity;
+        tetrominoNode.position = new Vector2(0, halfHeight);
+
+        switch(index)
+        {
+            // I : 하늘색
+            case 0:
+                color = new Color32(115, 251, 253, 255);
+                CreateTile(tetrominoNode, new Vector2(-2f, 0.0f), color);
+                CreateTile(tetrominoNode, new Vector2(-1f, 0.0f), color);
+                CreateTile(tetrominoNode, new Vector2(0f, 0.0f), color);
+                CreateTile(tetrominoNode, new Vector2(1f, 0.0f), color);
+                break;
+            
+            // J : 파란색
+            case 1:
+                color = new Color32(0, 33, 245, 255);
+                CreateTile(tetrominoNode, new Vector2(-1f, 0.0f), color);
+                CreateTile(tetrominoNode, new Vector2(0f, 0.0f), color);
+                CreateTile(tetrominoNode, new Vector2(1f, 0.0f), color);
+                CreateTile(tetrominoNode, new Vector2(-1f, 1.0f), color);
+                break;
+
+            // L : 주황색
+            case 2:
+                color = new Color32(243, 168, 59, 255);
+                CreateTile(tetrominoNode, new Vector2(-1f, 0.0f), color);
+                CreateTile(tetrominoNode, new Vector2(0f, 0.0f), color);
+                CreateTile(tetrominoNode, new Vector2(1f, 0.0f), color);
+                CreateTile(tetrominoNode, new Vector2(1f, 1.0f), color);
+                break;
+
+            // O : 노란색
+            case 3:
+                color = new Color32(255, 253, 84, 255);
+                CreateTile(tetrominoNode, new Vector2(0f, 0.0f), color);
+                CreateTile(tetrominoNode, new Vector2(1f, 0.0f), color);
+                CreateTile(tetrominoNode, new Vector2(0f, 0.0f), color);
+                CreateTile(tetrominoNode, new Vector2(1f, 1.0f), color);
+                break;
+
+            // S : 초록색
+            case 4:
+                color = new Color32(117, 250, 76, 255);
+                CreateTile(tetrominoNode, new Vector2(-1f, -1.0f), color);
+                CreateTile(tetrominoNode, new Vector2(0f, -1.0f), color);
+                CreateTile(tetrominoNode, new Vector2(0f, 0.0f), color);
+                CreateTile(tetrominoNode, new Vector2(1f, 0.0f), color);
+                break;
+
+            // T : 자주색
+            case 5:
+                color = new Color32(155, 47, 246, 255);
+                CreateTile(tetrominoNode, new Vector2(-1f, 0.0f), color);
+                CreateTile(tetrominoNode, new Vector2(0f, 0.0f), color);
+                CreateTile(tetrominoNode, new Vector2(1f, 0.0f), color);
+                CreateTile(tetrominoNode, new Vector2(0f, 1.0f), color);
+                break;
+
+            // Z : 빨강색
+            case 6:
+                color = new Color32(235, 51, 35, 255);
+                CreateTile(tetrominoNode, new Vector2(-1f, 1.0f), color);
+                CreateTile(tetrominoNode, new Vector2(0f, 1.0f), color);
+                CreateTile(tetrominoNode, new Vector2(0f, 0.0f), color);
+                CreateTile(tetrominoNode, new Vector2(1f, 0.0f), color);
+                break;
+        }
     }
 }
